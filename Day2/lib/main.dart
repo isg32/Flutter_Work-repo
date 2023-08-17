647c72e57c879766ae5b8856fc7e3f330a857a75
Sapan Gajjar <sapangajjar101105@gmail.com>
2023-08-17 17:40:10 +0530

    Update main.dart

--- Day2/lib/main.dart
+++ Day2/lib/main.dart
@@ -15,6 +15,23 @@ void main(){
           backgroundColor: Colors.green,
         ),
         body: Center(child: Text("This is body")),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "Cart"),
+            BottomNavigationBarItem(icon: Icon(Icons.taxi_alert), label: "Rent"),
+          ],
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              ListTile(leading: Icon(Icons.settings),title: Text("Settings")),
+              ListTile(leading: Icon(Icons.local_taxi),title: Text("History")),
+              ListTile(leading: Icon(Icons.shopping_bag),title: Text("View Cart"))
+            ],
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
             onPressed: (){
               print("Hello! How can i help You?");
